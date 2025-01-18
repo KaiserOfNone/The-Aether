6677cd2df4da7f058ae0bc5c4a1a97e32d52c9ff
bconlon <bconlon.gml@gmail.com>
2025-01-17 18:46:48 -0800

    fix: correct mixin priority for custom menu loading

--- src/main/java/com/aetherteam/aether/mixin/mixins/client/fabric/MinecraftMixin.java
+++ src/main/java/com/aetherteam/aether/mixin/mixins/client/fabric/MinecraftMixin.java
@@ -2,17 +2,23 @@ package com.aetherteam.aether.mixin.mixins.client.fabric;
 
 import com.aetherteam.aether.client.event.listeners.MenuListener;
 import com.aetherteam.aether.client.event.listeners.WorldPreviewListener;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
+@Mixin(value = Minecraft.class, priority = 444)
 public class MinecraftMixin {
-    @Inject(method = "setScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;added()V"))
-    private void openMenu(Screen guiScreen, CallbackInfo ci) {
+    @Inject(method = "setScreen(Lnet/minecraft/client/gui/screens/Screen;)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/BufferUploader;reset()V", shift = At.Shift.BEFORE),
+            slice = @Slice(
+                    from = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;shouldShowDeathScreen()Z"),
+                    to = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/BufferUploader;reset()V")))
+    public void setScreen(Screen guiScreen, CallbackInfo ci, @Local LocalRef<Screen> screen) {
         MenuListener.onGuiOpenHighest();
     }
 
